4a57cc0ecc6687cd7dbb083b4ac1c141eb0a56d7
Miguel <miguel.malo@snowstormtech.com>
2023-12-12 15:12:37 -0800

    Adding landing page to GitHub pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 
 function Hero() {
-    const vimeoEmbedUrl = "https://player.vimeo.com/video/892775103?autoplay=1&loop=1&muted=1&controls=0&title=0&byline=0&portrait=0";
+    const vimeoEmbedUrl = "https://player.vimeo.com/video/893901939?autoplay=1&loop=1&muted=1&badge=0&autopause=0&player_id=0&app_id=58479";
 
   return (
-    <div className="bg-deep-purple-accent-700 h-screen">
-        {/* <iframe 
+    <div className="bg-deep-purple-accent-700">
+        {/* Overlay */}
+        <div className="bg-overlay"></div>
+        <iframe 
             src={vimeoEmbedUrl}
             frameBorder="0"
             allow="autoplay; fullscreen; picture-in-picture"
@@ -15,24 +17,24 @@ function Hero() {
                 left: 0,
                 width: '100%',
                 height: '100%',
-                zIndex: -1,
+                zIndex: -2,
             }}
-        ></iframe> */}
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        ></iframe>
+        <div className="px-4 flex items-center h-[70vh] content-above-overlay py-16 mx-auto my-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
             <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
               <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12 group">
-                <h1 className="max-w-lg mb-6 font-sans text-8xl font-bold leading-none tracking-tight text-dark sm:text-6xl md:mx-auto transition-transform duration-200 group-hover:rotate-12">
+                <h1 className="max-w-lg mb-6 font-sans text-8xl font-bold leading-none tracking-tight text-white sm:text-6xl md:mx-auto transition-transform duration-200 group-hover:rotate-12">
                     Corporate Travel Simplified
                 </h1>
-                <p className="text-xl text-dark">
+                <p className="text-xl text-white">
                     Wherever you want to go next, see what’s possible with LetsGoThere.
                 </p>
               </div>
               <div>
                 <a
                   href="/"
-                  className="inline-flex items-center justify-center h-12 px-6 font-semibold tracking-wide text-teal-900 transition duration-200 shadow-md hover:text-deep-purple-900 bg-white hover:bg-deep-purple-accent-100 focus:shadow-outline focus:outline-none"
+                  className="inline-flex items-center justify-center h-12 px-6 font-semibold tracking-wide text-teal-900 transition duration-200 shadow-md hover:text-white bg-white hover:bg-primary focus:shadow-outline focus:outline-none"
                 >
                   Get started
                 </a>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,7 +11,7 @@ function Main() {
   return (
     <>
       {/* Info Banner */}
-      <div className="bg-secondary">
+      <div className="bg-secondary relative">
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
             <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
