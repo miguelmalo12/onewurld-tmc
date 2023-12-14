f06d06013667bc83da1cb1225cd62f87717e8b75
Miguel <miguel.malo@snowstormtech.com>
2023-12-14 14:21:37 -0800

    Updated footer to match all websites

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,124 +1,69 @@
-import logo from '../assets/main-logo.png';
+import logoSst from '../assets/logos/logo-sst.png';
+import logoOnewurld from '../assets/logos/logo-oneW.png';
+import logoAab from '../assets/logos/logo-aab.png';
+import logoYourwurld from '../assets/logos/logo-yourW.png';
 import arc from '../assets/img/arcgray.jpg';
 
 function Footer() {
   return (
     <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-      <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
-        <div className="sm:col-span-2 text-left">
-          <a
-            href="/"
-            aria-label="Go home"
-            title="Company"
-            className="inline-flex items-center"
-          >
-            <img
-              className="h-12 w-auto"
-              src={logo}
-              alt=""
-            />
-          </a>
-          <div className="mt-6 lg:max-w-sm">
-            <p className="text-sm text-gray-800">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam.
-            </p>
-            <p className="mt-4 text-sm text-gray-800">
-              Eaque ipsa quae ab illo inventore veritatis et quasi architecto
-              beatae vitae dicta sunt explicabo.
-            </p>
-          </div>
+      <h2 className='text-center max-w-lg mb-16 font-sans text-4xl font-bold leading-none tracking-tight text-gray-900 md:mx-auto'>Our Group Of Companies</h2>
+      
+      <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-1 lg:grid-cols-4">
+        {/* SST Logo */}
+        <div className='flex flex-col items-center text-center gap-2'>
+            <img className='h-12 mb-4' src={logoSst} alt="sst logo" />
+            <div className="border-t border-secondary mb-5 mx-auto w-1/3 text-center"></div>
+            <p>The perfect storm - Enabling our customers to leverage our technology, content and fulfillment services to thrive above others.</p>
+            <div className="text-center mt-4">
+                <a href="https://snowstormtech.com/" className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white bg-secondary transition duration-200 shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-primary focus:shadow-outline focus:outline-none" target="_blank">
+                    View Site
+                </a>
+            </div>
         </div>
-        <div className="space-y-2 text-sm text-left">
-          <p className="text-base font-bold tracking-wide text-gray-900">
-            Contacts
-          </p>
-          <div className="flex">
-            <p className="mr-1 text-gray-800">Phone:</p>
-            <a
-              href="tel:850-123-5021"
-              aria-label="Our phone"
-              title="Our phone"
-              className="transition-colors duration-300 text-primary hover:text-secondary"
-            >
-              850-123-5021
-            </a>
-          </div>
-          <div className="flex">
-            <p className="mr-1 text-gray-800">Email:</p>
-            <a
-              href="mailto:info@lorem.mail"
-              aria-label="Our email"
-              title="Our email"
-              className="transition-colors duration-300 text-primary hover:text-secondary"
-            >
-              info@lorem.mail
-            </a>
-          </div>
-          <div className="flex">
-            <p className="mr-1 text-gray-800">Address:</p>
-            <a
-              href="https://www.google.com/maps"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Our address"
-              title="Our address"
-              className="transition-colors duration-300 text-primary hover:text-secondary"
-            >
-              55 Water St, Vancouver, BC
-            </a>
-          </div>
+        {/* oneW Logo */}
+        <div className='flex flex-col items-center text-center gap-2'>
+            <img className='h-12 mb-4' src={logoOnewurld} alt="" />
+            <div className="border-t border-secondary mb-5 mx-auto w-1/3 text-center"></div>
+            <p>All-in-one platform that allows Leisure and Corporate Travel Agencies to compete and thrive in the marketplace.</p>
+            <div className="text-center mt-4">
+                <a href="https://onewurld.com/" className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white bg-secondary transition duration-200 shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-primary focus:shadow-outline focus:outline-none" target="_blank">
+                    View Site
+                </a>
+            </div>
         </div>
-        <div className="text-left">
-          <span className="text-base font-bold tracking-wide text-gray-900">
-            Social
-          </span>
-          <div className="flex items-center mt-1 space-x-3">
-            <a
-              href="/"
-              className="text-gray-500 transition-colors duration-300 hover:text-primary"
-            >
-              <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
-              </svg>
-            </a>
-            <a
-              href="/"
-              className="text-gray-500 transition-colors duration-300 hover:text-primary"
-            >
-              <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
-                <circle cx="15" cy="15" r="4" />
-                <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
-              </svg>
-            </a>
-            <a
-              href="/"
-              className="text-gray-500 transition-colors duration-300 hover:text-primary"
-            >
-              <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
-                <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
-              </svg>
-            </a>
-          </div>
-          <p className="my-4 text-sm text-gray-500">
-            Bacon ipsum dolor amet short ribs pig sausage prosciutto chicken
-            spare ribs salami.
-          </p>
-          <img className='h-10' src={arc} alt="arc certified" />
+        {/* AAB Logo */}
+        <div className='flex flex-col items-center text-center gap-2'>
+            <img className='h-12 mb-4' src={logoAab} alt="" />
+            <div className="border-t border-secondary mb-5 mx-auto w-1/3 text-center"></div>
+            <p>State of the art hotel technology with distribution through +60k agents across 5 continents generating over 8MM room nights.</p>
+            <div className="text-center mt-4">
+                <a href="https://aabhotels.com/" className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white bg-secondary transition duration-200 shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-primary focus:shadow-outline focus:outline-none" target="_blank">
+                    View Site
+                </a>
+            </div>
+        </div>
+        {/* yourWurld Logo */}
+        <div className='flex flex-col items-center text-center gap-2'>
+            <img className='h-12 mb-4' src={logoYourwurld} alt="" />
+            <div className="border-t border-secondary mb-5 mx-auto w-1/3 text-center"></div>
+            <p>Breakthrough package technology with world-class content and fulfillment services allowing hoteliers to monetize their direct traffic.</p>
+            <div className="text-center mt-4">
+                <a href="https://yourwurld.com/" className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white bg-secondary transition duration-200 shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-primary focus:shadow-outline focus:outline-none" target="_blank">
+                    View Site
+                </a>
+            </div>
         </div>
       </div>
+
+      {/* Copyright */}
       <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
         <p className="text-sm text-gray-600">
           © Copyright 2024 oneWurld, a division of Snowstorm Technologies. All rights reserved.
         </p>
         <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
           <li>
-            <a
-              href="/"
-              className="text-sm text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
-            >
-              F.A.Q
-            </a>
+            <img src={arc} alt="arc certified logo" className='h-8' />
           </li>
           <li>
             <a
