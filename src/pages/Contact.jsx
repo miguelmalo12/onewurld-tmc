144b04e7f7a65e13ac753774ed811daf4504002f
miguelmalo12 <miguel.malo@snowstormtech.com>
2024-02-21 11:19:59 -0800

    Changed Captcha Key

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,7 @@ import ReCAPTCHA from "react-google-recaptcha";
 function Contact() {
   const recaptchaRef = createRef();
   const [isCaptchaValid, setIsCaptchaValid] = useState(false);
-  const CAPTCHA_KEY = '6Le6qHQpAAAAAJr6dHVWFfQhK6vhCBEu8EgMw0st';
+  const CAPTCHA_KEY = '6Le6qHQpAAAAALFZUXshn0ZZUTzqt8DrBuJzEi8r';
 
   const [formData, setFormData] = useState({
     user_name: '',
